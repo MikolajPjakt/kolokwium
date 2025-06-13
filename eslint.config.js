f7d7b8019fb14b57c76075c0e0848b5208523bb6
Mikołaj Szymeczko <s24266@pjwstk.edu.pl>
2025-06-13 13:33:28 +0200

    Test pipeline na galezi dev 11

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+    {
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'commonjs',
+      },
+      env: {
+        node: true,
+      },
+      extends: 'eslint:recommended',
+    },
+  ];
